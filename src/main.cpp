b69dfdab2aa5663ef880121aa83df4477c8a3405
MartyLake <neogaldr@gmail.com>
2017-09-17 22:54:53 +0200

    Lesson 05 - Optimized Surface Loading and Soft Stretching

--- src/main.cpp
+++ src/main.cpp
@@ -69,13 +69,23 @@ bool init() {
 }
 
 SDL_Surface *loadSurface(std::string path) {
+  // The final optimized image
+  SDL_Surface *optimizedSurface = NULL;
   // Load image at specified path
   SDL_Surface *loadedSurface = SDL_LoadBMP(path.c_str());
   if (loadedSurface == NULL) {
     printf("Unable to load image %s! SDL Error: %s\n", path.c_str(),
            SDL_GetError());
+  } else {
+    // Convert surface to screen format
+    optimizedSurface =
+        SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+    if (optimizedSurface == NULL) {
+      printf("Unable to optimize image %s! SDL Error: %s\n", path.c_str(),
+             SDL_GetError());
+    } // Get rid of old loaded surface SDL_FreeSurface( loadedSurface );
   }
-  return loadedSurface;
+  return optimizedSurface;
 }
 
 bool loadMedia() {     // Loading success flag
@@ -166,7 +176,7 @@ int main(int argc, char *args[]) {
           }
         }
         // Apply the image
-        SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
+        SDL_BlitScaled(gCurrentSurface, NULL, gScreenSurface, NULL);
         // Update the surface
         SDL_UpdateWindowSurface(gWindow);
       }
